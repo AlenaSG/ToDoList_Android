d94013a33efaacee9a4f74a7a61fa0abd368e527
Guest User <Guest@epicodus-55.local>
2017-09-28 12:02:40 -0700

    add messages to the list

---
+++ app/src/main/java/com/example/guest/todolist/Intent_Constants.java
@@ -0,0 +1,11 @@
+package com.example.guest.todolist;
+
+/**
+ * Created by Guest on 9/28/17.
+ */
+
+public class Intent_Constants {
+    public final static int INTENT_REQUEST_CODE=1;
+    public final static int INTENT_RESULT_CODE=1;
+    public final static String INTENT_MESSAGE_FIELD="message_field";
+}
