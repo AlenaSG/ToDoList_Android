4e14c109cc232d71e8189697180405aca1d37a77
Guest User <Guest@epicodus-55.local>
2017-09-28 15:09:32 -0700

    add method to save data

--- app/src/main/java/com/example/guest/todolist/MainActivity.java
+++ app/src/main/java/com/example/guest/todolist/MainActivity.java
@@ -1,19 +1,25 @@
 package com.example.guest.todolist;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<String> arrayList;
     ArrayAdapter<String> arrayAdapter;
     String messageText;
+    int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,41 @@ public class MainActivity extends AppCompatActivity {
         arrayList = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, arrayList);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent intent = new Intent();
+                intent.setClass(MainActivity.this, EditMessageClass.class);
+                intent.putExtra(Intent_Constants.INTENT_MESSAGE_DATA,arrayList.get(position).toString());
+                intent.putExtra(Intent_Constants.INTENT_ITEM_POSITION, position);
+                startActivityForResult(intent,Intent_Constants.INTENT_REQUEST_CODE);
+        }
+    });
+
+        try {
+            Scanner sc = new Scanner(openFileInput("Todo.txt"));
+            while(sc.hasNextLine()) {
+                String data = sc.nextLine();
+            arrayList.add(data);
+            }
+            sc.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+            PrintWriter pw = new PrintWriter(openFileOutput("Todo.txt", Context.MODE_PRIVATE));
+            for(String data : arrayList) {
+                pw.println(data);
+            }
+            pw.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finish();
     }
 
     public void onClick(View v){
@@ -38,6 +79,12 @@ public class MainActivity extends AppCompatActivity {
         if(resultCode==Intent_Constants.INTENT_REQUEST_CODE) {
             messageText = data.getStringExtra(Intent_Constants.INTENT_MESSAGE_FIELD);
             arrayList.add(messageText);
+            arrayAdapter.notifyDataSetChanged();
+        } else if (resultCode==Intent_Constants.INTENT_REQUEST_CODE_TWO){
+            messageText = data.getStringExtra(Intent_Constants.INTENT_CHANGED_MESSAGE);
+            position = data.getIntExtra(Intent_Constants.INTENT_ITEM_POSITION, -1);
+            arrayList.remove(position);
+            arrayList.add(position,messageText);
             arrayAdapter.notifyDataSetChanged();
         }
     }
